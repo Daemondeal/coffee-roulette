888ddf83b49fbacaa41557114f44d97fe69b8bca
Daemondeal <daemondaeal@gmail.com>
2025-11-30 17:42:40 +0100

    fool pythonanywhere

--- coffee_roulette/app.py
+++ coffee_roulette/app.py
@@ -1,3 +1,4 @@
+from socket import gethostname 
 import os
 from flask import Flask
 from coffee_roulette.db import init_db
@@ -10,8 +11,15 @@ def create_app():
     app.secret_key = os.getenv("SECRET_KEY")
     return app
 
-def main():
+def main(run_app):
     init_db()
     load_dotenv()
     app = create_app()
-    app.run()
+    if run_app:
+        app.run()
+
+
+if __name__ == "__main__":
+    run_app = 'liveconsole' not in gethostname()
+    main(run_app)
+
